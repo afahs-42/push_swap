bf2f50b8e34e832017385fa06e71087b1a899c71
Ahmd Fahs <afahs@c1r7s1.42beirut.com>
2025-06-23 06:54:19 +0300

    finilizing

---
+++ push_swap.h
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afahs <afahs@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/15 19:40:28 by afahs             #+#    #+#             */
+/*   Updated: 2025/06/23 06:15:51 by afahs            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_stack
+{
+	int				value;
+	int				index;
+	int				pos;
+	int				target_pos;
+	int				cost_a;
+	int				cost_b;
+	struct s_stack	*next;
+}	t_stack;
+
+// /ops
+void	sa(t_stack	**stack_a);
+void	sb(t_stack	**stack_b);
+void	ss(t_stack	**stack_a,	t_stack	**stack_b);
+void	pa(t_stack	**stack_a,	t_stack	**stack_b);
+void	pb(t_stack	**stack_a,	t_stack	**stack_b);
+void	ra(t_stack	**stack_a);
+void	rb(t_stack	**stack_b);
+void	rr(t_stack	**stack_a,	t_stack	**stack_b);
+void	rra(t_stack	**stack_a);
+void	rrb(t_stack	**stack_b);
+void	rrr(t_stack	**stack_a,	t_stack	**stack_b);
+
+// stack_utils.c
+t_stack	*stack_new(int value);
+void	stack_add_bottom(t_stack	**stack,	t_stack	*new);
+t_stack	*stack_get_last(t_stack	*stack);
+int		stack_size(t_stack	*stack);
+void	free_stack(t_stack	**stack);
+
+// sort_utls.c
+void	push_chunks(t_stack **stack_a, t_stack **stack_b, int chunk_size);
+void	sort_three(t_stack	**stack);
+void	sort(t_stack	**stack_a,	t_stack	**stack_b);
+
+// position_utils.c
+void	assign_index(t_stack	*stack_a, int stack_size);
+void	get_position(t_stack	**stack);
+void	get_target_position(t_stack	**stack_a,	t_stack	**stack_b);
+void	get_cost(t_stack	**stack_a,	t_stack	**stack_b);
+
+// move_utils.c
+void	do_cheapest_move(t_stack	**stack_a,	t_stack	**stack_b);
+
+// check_utils.c
+int		is_number(char	*str);
+int		has_duplicates(t_stack	*stack);
+t_stack	*parse_args(int c, char **v);
+
+// random_utils.c
+int		is_sorted(t_stack	*stack);
+long	int	ft_atoi(const	char	*str);
+int		abs_value(int n);
+void	shift_stack(t_stack	**stack_a);
+
+#endif
---
+++ utils/sort_utils.c
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afahs <afahs@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/22 23:39:55 by afahs             #+#    #+#             */
+/*   Updated: 2025/06/23 05:58:01 by afahs            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+//tool[0] = chunk_max ; tool[1] = rotations ; tool[2] = size_a ;
+static void	push_chunks_helper(int *tools, int chunk_size)
+{
+	if (tools[2] >= tools[3])
+	{
+		tools[1] += chunk_size;
+		tools[0] = 0;
+		tools[2] = 0;
+	}
+	if (tools[0] == chunk_size)
+	{
+		tools[1] += chunk_size;
+		tools[0] = 0;
+	}
+}
+
+void	push_chunks(t_stack **stack_a, t_stack **stack_b, int chunk_size)
+{
+	int	*tools;
+
+	tools = malloc(sizeof(int) * 4);
+	tools[0] = 0;
+	tools[1] = chunk_size;
+	while (stack_size(*stack_a) > 3)
+	{
+		tools[2] = 0;
+		tools[3] = stack_size(*stack_a);
+		if ((*stack_a)->index <= tools[1])
+		{
+			pb(stack_a, stack_b);
+			tools[0]++;
+			if (stack_size(*stack_b) > 1
+				&& (*stack_b)->index < tools[1] - chunk_size / 2)
+				rb(stack_b);
+		}
+		else
+		{
+			ra(stack_a);
+			tools[2]++;
+		}
+		push_chunks_helper(tools, chunk_size);
+	}
+	free(tools);
+}
+
+static void	push_init(t_stack **stack_a, t_stack **stack_b)
+{
+	int	size;
+	int	chunk_size;
+
+	size = stack_size(*stack_a);
+	assign_index(*stack_a, size);
+	if (size <= 100)
+		chunk_size = 20;
+	else if (size <= 500)
+		chunk_size = 45;
+	else
+		chunk_size = 75;
+	push_chunks(stack_a, stack_b, chunk_size);
+	while (stack_size(*stack_a) > 3)
+		pb(stack_a, stack_b);
+	sort_three(stack_a);
+}
+
+void	sort_three(t_stack **a)
+{
+	int	first;
+	int	second;
+	int	third;
+
+	first = (*a)->value;
+	second = (*a)->next->value;
+	third = (*a)->next->next->value;
+	if (first > second && second < third && first < third)
+		sa(a);
+	else if (first > second && second > third)
+	{
+		sa(a);
+		rra(a);
+	}
+	else if (first > second && second < third && first > third)
+		ra(a);
+	else if (first < second && second > third && first < third)
+	{
+		sa(a);
+		ra(a);
+	}
+	else if (first < second && second > third && first > third)
+		rra(a);
+}
+
+void	sort(t_stack **stack_a, t_stack **stack_b)
+{
+	int	size;
+
+	size = stack_size(*stack_a);
+	if (size == 2 && !is_sorted(*stack_a))
+		sa(stack_a);
+	else if (size == 3)
+		sort_three(stack_a);
+	else if (size > 3)
+	{
+		push_init(stack_a, stack_b);
+		while (*stack_b)
+		{
+			get_target_position(stack_a, stack_b);
+			get_cost(stack_a, stack_b);
+			do_cheapest_move(stack_a, stack_b);
+		}
+		if (!is_sorted(*stack_a))
+			shift_stack(stack_a);
+	}
+}
